f9bcb0be1ad20b3032a7bcd81bbdc8a7d670a0d6
wkcn <wkcn@live.cn>
2018-04-28 05:39:16 +0000

    add req for roi align

--- roi_align_op.py
+++ roi_align_op.py
@@ -9,18 +9,34 @@ class ROIAlignOP(mx.operator.CustomOp):
         self.spatial_scale = spatial_scale
         self.sampling_ratio = sampling_ratio
     def forward(self, is_train, req, in_data, out_data, aux):
+        if req[0] == 'null':
+            return
         data = in_data[0]
         rois = in_data[1]
         out = out_data[0]
-        roi_align_op.forward(data, rois, self.pooled_size, self.spatial_scale, self.sampling_ratio, out)
+        if req[0] == 'add':
+            out_temp = self.get_ndarray_temp(out) 
+            roi_align_op.forward(data, rois, self.pooled_size, self.spatial_scale, self.sampling_ratio, out_temp)
+            out[:] += out_temp
+        else:
+            roi_align_op.forward(data, rois, self.pooled_size, self.spatial_scale, self.sampling_ratio, out)
 
     def backward(self, req, out_grad, in_data, out_data, in_grad, aux):
+        if req[0] == 'null':
+            return
         data = in_data[0]
         rois = in_data[1]
         data_diff = in_grad[0]
         top_diff = out_grad[0]
-        roi_align_op.backward(data, rois, top_diff, self.pooled_size, self.spatial_scale, data_diffo)
+        if req[0] == 'add':
+            data_diff_temp = self.get_ndarray_temp(data_diff) 
+            roi_align_op.backward(data, rois, top_diff, self.pooled_size, self.spatial_scale, data_diff_temp)
+            data_diff[:] += data_diff_temp
+        else:
+            roi_align_op.backward(data, rois, top_diff, self.pooled_size, self.spatial_scale, data_diff)
         self.assign(in_grad[1], req[1], 0)
+    def get_ndarray_temp(self, out):
+        return mx.nd.empty(shape = out.shape, dtype = out.dtype, ctx = out.context)
 
 @mx.operator.register('ROIAlign')
 class ROIAlignProp(mx.operator.CustomOpProp):
